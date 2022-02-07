76fcadc06dd87ac598056466313cbdf62760255b
Patrockanite <patrickannoot@orange.fr>
2022-02-07 21:10:35 +0100

    ajout js pour XSmall

---
+++ js/camille.js
@@ -0,0 +1,18 @@
+(function(){
+    var largeurEcran = screen.width ;
+    var pour_photoXs = document.getElementById("XSmall");
+    var pour_photoXXXs = document.getElementById("XXXSmall");
+    var imageSmall = document.createElement("img");
+
+    if(largeurEcran>401 & largeurEcran<768){
+        imageSmall.src="../img/slider/XSmall.jpg";
+        pour_photoXs.appendChild(imageSmall);
+        
+        
+    }
+    if(largeurEcran>10 & largeurEcran<400){
+        imageSmall.src="../img/slider/2XSmall.jpg";
+        pour_photoXXXs.appendChild(imageSmall);
+        
+    }
+})()
