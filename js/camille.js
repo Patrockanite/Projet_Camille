c25d9c6a1be172c68db68fefb9bd172be1a2fb1c
Patrockanite <patrickannoot@orange.fr>
2022-02-08 09:19:13 +0100

    jquery

--- js/camille.js
+++ js/camille.js
@@ -1,4 +1,6 @@
-(function(){
+/*ce code fonctionne en mode inspection mais pas en mode navigateur ! Pourquoi ?*/
+/*Je ne sais pas alors je vais essayer avec du JQuery dessous*/
+/*(function(){
     var largeurEcran = screen.width ;
     var pour_photoXs = document.getElementById("XSmall");
     var pour_photoXXXs = document.getElementById("XXXSmall");
@@ -15,4 +17,34 @@
         pour_photoXXXs.appendChild(imageSmall);
         
     }
-})()
+})()*/
+/*********** JQuery pour afficher les photos si l'écran est petit *************/
+var $window = $(window);
+$(document).ready(function(){ 
+
+    /**************** on cache les blocs XSmall et XXXSmall ***********/
+    $("#XSmall").hide();
+    $("#XXXSmall").hide();
+    $windowWidth = $(this).width();
+
+
+    if($windowWidth>400 & $windowWidth<768){
+        $("#XSmall").show();
+        console.log($windowWidth + "Petite");
+    }
+
+    if($windowWidth>10 & $windowWidth<400){
+        $("#XXXSmall").show();
+        console.log($windowWidth + "Très petite");
+    }
+    
+   /* $(window).on('resize', function() {
+        $windowWidth = $(this).width();
+     
+        console.log($windowWidth);
+    });*/     
+    
+    
+    
+    
+}) ; //fin du ready
