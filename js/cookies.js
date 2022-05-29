e95cda1f72d2356e5e29d1bf4285240adf5fc42d
Patrockanite <patrickannoot@orange.fr>
2022-05-29 10:02:56 +0200

    correction nom therapeuthe

---
+++ js/cookies.js
@@ -0,0 +1,11 @@
+// Gestion des cookies pour la RPDL
+        window.axeptioSettings = {
+          clientId: "628fb754a616c7d4939a35f9",
+          cookiesVersion: "care-in-coloc-fr",
+        };
+         
+        (function(d, s) {
+          var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+          e.async = true; e.src = "//static.axept.io/sdk.js";
+          t.parentNode.insertBefore(e, t);
+        })(document, "script");
